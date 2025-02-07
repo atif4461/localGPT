46f014290815342e45a4139c5f518ea580df62c4
atif4461 <mohammadatif4461@gmail.com>
2025-02-06 19:10:41 -0500

    reverted MODELS_PATH

--- constants.py
+++ constants.py
@@ -17,7 +17,7 @@ SOURCE_DIRECTORY = f"{ROOT_DIRECTORY}/SOURCE_DOCUMENTS"
 
 PERSIST_DIRECTORY = f"{ROOT_DIRECTORY}/DB"
 
-MODELS_PATH = "/home/atif/localGPT/models"
+MODELS_PATH = "./models"
 
 # Can be changed to a specific number
 INGEST_THREADS = os.cpu_count() or 8
